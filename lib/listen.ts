ac4b0477218a7465fb5f8edafb1d0f6670008c3c
Ali Amjad <66186390+Ali-A-Koye@users.noreply.github.com>
2022-09-07 14:12:50 +0300

    Adding Listening Functionality

--- lib/listen.ts
+++ lib/listen.ts
@@ -1,5 +1,27 @@
-const listen = () => {
-    console.log('listen');
-};
+import ListenType from "../types/listen.d";
 
+const listen = (values: ListenType) => {
+  const { channel, callback } = values;
+
+  channel.assertQueue((process.env.microservice_origin = __dirname), {
+    durable: false,
+  });
+  channel.prefetch(1);
+
+  channel.consume(
+    (process.env.microservice_origin = __dirname),
+    async (msg) => {
+      if (msg !== null) {
+        const data = msg.content.toString();
+        await callback(data);
+        channel.sendToQueue(msg.properties.replyTo, Buffer.from(data), {
+          correlationId: msg.properties.correlationId,
+        });
+
+        channel.ack(msg);
+      }
+    },
+    { noAck: false }
+  );
+};
 export default listen;
